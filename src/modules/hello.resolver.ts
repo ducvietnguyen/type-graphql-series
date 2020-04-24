8444d04a37d1c14c22b9b161bdf4c83d683286db
NGUYEN DUC VIET <ducvietnguyen195@live.com>
2020-04-24 14:40:31 +0700

    authorization

--- src/modules/hello.resolver.ts
+++ src/modules/hello.resolver.ts
@@ -1,7 +1,11 @@
-import {Resolver, Query } from "type-graphql";
+import {Resolver, Query, UseMiddleware } from "type-graphql";
+import { isAuth } from "./middlewares/isAuth";
 
 @Resolver()
 class HelloResolver {
+
+  // @Authorized()
+  @UseMiddleware(isAuth)
   @Query(() => String, {name: 'sayHelloWorld', description:'This is the first function of the app'})
   async helloWorld() {
     return "Hello World!";
